2fa0f4c7d2bd601d834a6cfb5615fb06cdd96676
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-11 16:17:17 +0600

    add login form

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,7 +1,15 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment }) => {
+const BookingModal = ({ date, treatment, setTreatment }) => {
     const { name, slots } = treatment;
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const slot = event.target.slot.value;
+        console.log(slot);
+        setTreatment(null)
+    }
 
     return (
         <div>
@@ -10,12 +18,17 @@ const BookingModal = ({ treatment }) => {
                 <div className="modal-box">
                     <label for="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-center">Book Appointment : {name}</h3>
-                    <form className='grid grid-cols-1 gap-4 mt-3 justify-items-center'>
-                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                    <form onSubmit={handleSubmit} className='grid grid-cols-1 gap-4 mt-3 justify-items-center'>
+                        <input type="text" disabled value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" />
+                        <select name='slot' class="select select-bordered w-full max-w-xs">
+                            {
+                                slots.map(slot => <option value={slot}>{slot}</option>)
+                            }
+
+                        </select>
+                        <input type="text" name='name' placeholder="Full Name" class="input input-bordered w-full max-w-xs" />
+                        <input type="number" name='number' placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
+                        <input type="email" name='email' placeholder="Email Address" class="input input-bordered w-full max-w-xs" />
                         <input type="submit" value='submit' class="input input-bordered w-full max-w-xs
                         btn btn-primary uppercase font-bold text-white bg-gradient-to-r from-secondary to-primary" />
                     </form>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div class="hero min-h-screen bg-base-200">
+            <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <div class="form-control">
+                        <h1 className='text-center text-xl'>Login</h1>
+                        <label class="label">
+                            <span class="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="email" class="input input-bordered" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Password</span>
+                        </label>
+                        <input type="text" placeholder="password" class="input input-bordered" />
+                        <label class="label">
+                            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div class="form-control">
+                        <button class="btn bg-accent text-white text-md">Login</button>
+                    </div>
+                    <div className='text-sm text-center'>
+                        <p>New to Doctors Portal?
+                            <Link to={'/signup'} className='text-secondary ml-2 hover:link'>Create new account</Link>
+                        </p>
+                    </div>
+
+                    <div class="flex flex-col w-full border-opacity-50">
+                        <div class="divider">OR</div>
+                    </div>
+                    <div class="form-control">
+                        <button class="btn bg-white text-accent text-md hover:bg-white text-accent">Continue With Google</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
